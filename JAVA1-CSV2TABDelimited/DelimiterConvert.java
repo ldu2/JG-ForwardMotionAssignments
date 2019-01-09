d1d33373a087d4daa3749d023e80f00cb0e4e7a8
LuYangDu <luyangdu@udows.localdomain>
2019-01-09 14:07:49 -0500

    Finish JAVA1 assignment using stream

--- JAVA1-CSV2TABDelimited/DelimiterConvert.java
+++ JAVA1-CSV2TABDelimited/DelimiterConvert.java
@@ -1,8 +1,14 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class DelimiterConvert{
 
@@ -22,23 +28,61 @@ public class DelimiterConvert{
 
 		}
 
+		public String getSchool(){
+			return _school;
+		}
+
 		public String toString(){
-			return _id + "\t" +_name+"\t"+_school; 
+			return _id + "\t" +_name+"\t"+_school+"\n";
 		}
 
 	}
 
 	static void readData() throws FileNotFoundException{
 		Scanner scanner = new Scanner(new File("data.csv"));
-		scanner.useDelimiter(",");
 		while(scanner.hasNext()){
-		    System.out.print(scanner.next());
+			String[] dt = scanner.nextLine().split(",",0);
+			Student s = new Student(dt[0],dt[1],dt[2]);
+			students.add(s);
 		}
 		scanner.close();
 	}
 
-	public static void main(String[] args) throws FileNotFoundException{
-		readData();
+	static void writeAllData() throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter("allOutput.txt"));
+		
+		List<String> infoList = students.stream().map(stu -> stu.toString()).collect(Collectors.toList());
+		
+		writer.write(String.join("",infoList));
+
+		writer.close();
+	}
+
+	static void writeSchoolData() throws IOException {
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter("schoolOutput.txt"));
+
+		List<String> schoolList = students.stream().map(stu -> stu.getSchool()).collect(Collectors.toList());
+
+		writer.write(String.join("\t",schoolList));
+		
+		writer.close();
+	}
+
+	public static void main(String[] args){
+		try{
+			students = new ArrayList<Student>();
+			readData();
+			writeAllData();
+			writeSchoolData();
+
+		}catch(FileNotFoundException e){
+			System.out.println("Please make sure you have a data file with the name data.csv and the delimiters are commas(,)");
+		}
+		catch(IOException e){
+			System.out.println("Please make sure you have a data file with the name data.csv and the delimiters are commas(,)");
+		}
+	
 	}
 
 
